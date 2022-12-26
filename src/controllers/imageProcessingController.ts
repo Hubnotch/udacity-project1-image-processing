22279dc15310348a3ab17f370d947f160cb40ea7
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2022-12-26 07:59:28 +0100

    functionality to reaize inage

--- src/controllers/imageProcessingController.ts
+++ src/controllers/imageProcessingController.ts
@@ -1,27 +1,32 @@
-import * as fs from "fs";
-import sharp from 'sharp'
-import { Request, Response } from 'express'
+import { promises as fsPromises } from 'fs';
+import fs from 'fs';
+import sharp from 'sharp';
+import { Request, Response } from 'express';
+import resizeImage from "../utility/imageProcessingFunc";
+import path from 'path';
 
-export const imageProcessing = async (req: Request, res: Response) => {
+
+export const imageProcessing = async (req: Request, res: Response): Promise<void> => {
     const filename = req.query.filename;
     const width = (Number(req.query.width) as unknown) as number;
     const height = (Number(req.query.height) as unknown) as number;
 
-    
     const imagePath = `${process.cwd()}/images/${req.query.filename}.jpg`;
+try {
     if (!filename || !width || !height) {
         res.status(404).send('Incorrect image parameters');
         return;
-     }
-     
-    await sharp(imagePath)
-        .resize(width, height)
-        .toBuffer()
-        .then((data: Buffer) => {
-            res.set('Content-Type', 'image/jpg')
-            res.set('Cache-Control', 'public, max-age=31536000')
-            res.send(data)
-        }).catch((err: Error) => {
-            res.status(500).json({ error: err.message })
-        })
+    }
+
+    const resizedImagePath = `images/resized/${filename}x${width}x${height}.jpg`
+
+    if (!fs.existsSync(resizedImagePath)) {
+        const resizedImage = await resizeImage(imagePath, width, height);
+        await fsPromises.writeFile(resizedImagePath, resizedImage)
+    }
+    res.sendFile(path.resolve(resizedImagePath))
+} catch (error) {
+    res.status(400).send('Image not found!');
 }
+   
+};
